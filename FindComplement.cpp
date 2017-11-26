3f44116e452f2b36b07caf5d41086d860aa2aaa2
929856540.com@qq.com <929856540@qq.com>
2017-11-26 01:45:08 -0800

    完成求补数

--- FindComplement.cpp
+++ FindComplement.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
+#include<limits>
 using namespace std;
 
-int FindComplement(int num)
+int FindComplement1(int num)
 {
     bool start = false;
     for(int i = 31;i >= 0;--i)
@@ -9,16 +10,39 @@ int FindComplement(int num)
         if(num & (1 << i))
             start = true;
         if(start)
-            num ^= (1 << i);
+        num ^= (1 << i);
     }
     return num;
 }
 
+int FindComplement2(int num)
+{
+    int mask = INT32_MAX;
+    while(mask & num)
+    {
+        mask <<= 1;
+    }
+    return ~mask & ~num;
+}
+
+int FindComplement3(int num)
+{
+    return (1 - num%2)+2*(num <= 1?0:FindComplement3(num/2));
+}
+
 int main()
 {
-    int num1 = 5;
-    cout<<FindComplement(num1)<<endl;
+    int num1 = 512;
+    cout<<FindComplement1(num1)<<endl;
     int num2 = 1;
-    cout<<FindComplement(num2)<<endl;
+    cout<<FindComplement1(num2)<<endl;
+
+    cout<<FindComplement2(num1)<<endl;
+    cout<<FindComplement2(num2)<<endl;
+
+    cout<<FindComplement3(num1)<<endl;
+    cout<<FindComplement3(num2)<<endl;
+
+    cout<<INT32_MAX<<endl;
     return 0;
 }
