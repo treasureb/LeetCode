31150a8c299f6f25bd339227687b1bc837d0fe6e
maple <929856540@qq.com>
2018-03-12 22:56:02 -0700

    求二叉树的高度

---
+++ TreeQuestion/TreeDepth.cc
@@ -0,0 +1,20 @@
+#include"Tree.h"
+
+/*
+ *   1、求根的高度，就是求左右子树中较大的+1
+ *   2、求其左右子树的高度的方法与求根相同
+ */
+
+
+int TreeDepth(BinaryTreeNode* Root)
+{
+    if(Root == nullptr)
+        return 0;
+
+    //分别求其左右子树的高度
+    int left = TreeDepth(Root->_left);
+    int right = TreeDepth(Root->_right);
+
+    //上层的高度为其左右子树较大的+1
+    return left > right?(left+1):(right+1);
+}
