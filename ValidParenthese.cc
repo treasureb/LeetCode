ecd441b118f46dd3fc1cffbd6888d10aaf5ff999
maple <929856540@qq.com>
2018-03-17 20:13:13 -0700

    括号匹配，采用栈的先进后出

---
+++ ValidParenthese.cc
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<string>
+#include<stack>
+
+using namespace std;
+class Solution{
+public:
+    bool isValid(std::string s){
+        size_t length = s.size();
+        if(length % 2 == 1)
+            return false;
+
+        stack<char> st;
+        for(size_t i = 0;i < length;++i){
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{')
+                st.push(s[i]);
+            else{
+                if(st.empty())
+                    return false;
+                char top = st.top();
+                switch(s[i]){
+                case ')':
+                    if(top != '(')
+                        return false;
+                    break;
+                case ']':
+                    if(top != '[')
+                        return false;
+                    break;
+                case '}':
+                    if(top != '{')
+                        return false;
+                    break;
+                default:
+                    break;
+                }
+                st.pop();
+            }
+
+        }
+        return st.empty();
+    }
+};
