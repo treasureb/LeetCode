09cb6e3171722dfc6b84b5bd308f1c1ce3f068d1
maple <929856540@qq.com>
2018-03-21 00:53:46 -0700

    队列的最大值，采用两个双端队列实现

---
+++ StackAndQueue/QueueWithMax.cc
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<exception>
+#include<stdexcept>
+#include<queue>
+
+using namespace std;
+
+template <class T>
+class QueueWithMax
+{
+public:
+    QueueWithMax()
+    :currentIndex(0)
+    {}
+
+    void push_back(T number)
+    {
+        while(!maximums.empty() && number >= maximums.back().number)
+            maximums.pop_back();
+
+        InternalData internalData{number,currentIndex};
+        data.push_back(number);
+        maximums.push_back(internalData);
+
+        ++currentIndex;
+    }
+
+    void pop_front()
+    {
+        if(maximums.empty())
+            throw new exception("queue is empty");
+
+        if(maximums.front().index == data.front().index)
+            maximums.pop_front();
+
+        data.pop_front;
+    }
+
+    T max() const
+    {
+        if(maximums.empty())
+            throw new exception("queue is empty");
+
+        return maximums.front().number;
+    }
+private:
+    struct InternalData
+    {
+        T number;
+        int index;
+    };
+
+    deque<InternalData> data;
+    deque<InternalData> maximums;
+    int currentIndex;
+};
