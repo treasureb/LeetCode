4d2cdea15e5fbd91439c3c9553726e6b508d8583
maple <929856540@qq.com>
2018-04-19 19:27:52 -0700

    重建二叉树

---
+++ TreeQuestion/RebuildTree.cc
@@ -0,0 +1,44 @@
+#include<iostream>
+#include"Tree.h"
+#include<vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+   Node* reconstruct_binarytree(vector<int> pre,vector<int> in)
+   {
+       int in_size = in.size();
+       if(in_size == 0)
+           return nullptr;
+
+       vector<int> pre_left,pre_right,in_left,in_right;
+       int root = pre[0];//前序的第一个是根节点
+       Node* node = new Node(root);
+       int p = 0;
+       for(;p < in_size;++p){//在中序中找到根节点
+           if(in[p] == root)
+               break;
+       }
+
+       for(int i = 0;i < in_size;++i){//划分子区间
+           if(i < p){
+               in_left.push_back(in[i]);
+               pre_left.push_back(pre[i+1]);
+           }else if(i > p){
+               in_right.push_back(in[i]);
+               pre_right.push_back(pre[i]);
+           }
+       }
+
+       node->_left = reconstruct_binarytree(pre_left,in_left);
+       node->_left = reconstruct_binarytree(pre_right,in_right);
+
+       return node;
+   }
+};
+int main()
+{
+    return 0;
+}
