afd9e5874faea383932c68f63e9e5e4161b502fe
maple <929856540@qq.com>
2018-04-20 02:17:40 -0700

    非递归实现求镜像树

--- TreeQuestion/Mirror.cc
+++ TreeQuestion/Mirror.cc
@@ -1,6 +1,8 @@
 #include"Tree.h"
+#include<stack>
 
-void MirrorTree(BinaryTreeNode* Root)
+//递归求一个树的镜像
+void MirrorTree(Node* Root)
 {
     if(Root == nullptr)
         return;
@@ -10,7 +12,7 @@ void MirrorTree(BinaryTreeNode* Root)
 
     //走到这里就可以进行交换了。
     
-    BinaryTreeNode* tmp = Root->_left;
+    Node* tmp = Root->_left;
     Root->_left = Root->_right;
     Root->_right = tmp;
 
@@ -18,4 +20,25 @@ void MirrorTree(BinaryTreeNode* Root)
         MirrorTree(Root->_left);
     if(Root->_right)
         MirrorTree(Root->_right);
+}
+
+//非递归求树的镜像
+void mirror_tree(Node* root)
+{
+    if(root == nullptr)
+        return;
+
+    stack<Node*> s;
+    s.push(root);
+    while(!s.empty()){
+        Node* top = s.top();
+        s.pop();
+        if(top->_left != nullptr || top->_right != nullptr)
+            swap(top->_left,top->_right);
+
+        if(top->_left)
+            s.push(top->_left);
+        if(top->_right)
+            s.push(top->_right);
+    }
 }
