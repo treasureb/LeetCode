b76c8b5c3fed1e5c6d39694eaabe7bb2ebe6207f
maple <929856540@qq.com>
2018-04-20 17:38:44 -0700

    层序遍历二叉树，返回vector

--- TreeQuestion/LevelOrder.cc
+++ TreeQuestion/LevelOrder.cc
@@ -1,8 +1,9 @@
 #include<iostream>
+#include<vector>
 #include<queue>
 
 #include"Tree.h"
-
+using namespace std;
 
 void LevelOrder(Node* Head)
 {
@@ -22,3 +23,33 @@ void LevelOrder(Node* Head)
             s.push(Front->_right);
     }
 }
+
+
+class Solution
+{
+public:
+    vector<int> level_order(Node* root)
+    {
+        vector<int> res;
+        queue<Node*> q;
+        if(root == nullptr)
+            return res;
+
+        q.push(root);
+        res.push_back(root->_value);
+        while(!q.empty()){
+            Node* front = q.front();
+            q.pop();
+            if(front->_left != nullptr){
+                q.push(front->_left);
+                res.push_back(front->_left->_value);
+            }
+            if(front->_right != nullptr){
+                q.push(front->_right);
+                res.push_back(front->_right->_value);
+            }
+        }
+        return res;
+    }
+    
+};
