deeb7ca668756fd46614cb2a85d284171ef21db8
maple <929856540@qq.com>
2018-05-28 08:35:18 -0700

    求丑数

--- Digit/UglyNumber.cc
+++ Digit/UglyNumber.cc
@@ -1,4 +1,5 @@
 #include<ostream>
+#include<vector>
 
 using namespace std;
 
@@ -81,6 +82,28 @@ int GetUglyNumber2(int index)
     delete[] UglyNumber;
     return ugly;
 }
+
+class Solution{
+public:
+    int GetUglyNumber(int index){
+        if(index < 7)
+            return index;
+
+        vector<int> res(index);
+        res[0] = 1;
+        int t2 = 0,t3 = 0,t5 = 0;
+        for(int i = 0;i < index;i++){
+            res[i] = min(res[t2] * 2,min(res[t3] * 3,res[t5] * 5));
+            if(res[i] == res[t2] * 2)
+                t2++;
+            if(res[i] == res[t3] * 3)
+                t3++;
+            if(res[i] == res[t5] * 5)
+                t5++;
+        }
+        return res[index - 1];
+    }
+};
 
 int Min(int number1,int number2,int number3)
 {
