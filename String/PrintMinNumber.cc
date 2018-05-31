ed33b57bc9555ebd45790fcbf110e8bb3d910d0e
maple <929856540@qq.com>
2018-05-31 08:40:58 -0700

    求最小数,添加注释

--- String/PrintMinNumber.cc
+++ String/PrintMinNumber.cc
@@ -8,13 +8,17 @@ class Solution{
 public:
     string printf_min_number(vector<int> numbers){
         string answer = "";
+        //自定义排序规则
         sort(numbers.begin(),numbers.end(),cmp);
+        //然后直接进行字符串相加
         for(int i = 0;i < (int)numbers.size();++i){
             answer+=to_string(numbers[i]);
         }
         return answer;
     }
 private:
+    //这里的比较函数必须定义为static
+    //两个数相互组成两个字符串，然后比较
     static bool cmp(int a,int b){
         string A="";
         string B="";
