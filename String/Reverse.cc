763c714a3f151fe04a8f0b6ffa13ac70dfea574a
maple <929856540@qq.com>
2018-06-01 18:45:08 -0700

    C语言实现逆置字符串,左旋问题

--- String/Reverse.cc
+++ String/Reverse.cc
@@ -1,74 +1,73 @@
 #include<iostream>
 #include<string>
-
 using namespace std;
 //整体反转
-void Reverse(char* Begin,char* End)
-{
-    if(nullptr == Begin || nullptr == End)
-        return;
-
-    while(Begin < End){
-         char tmp = *Begin;
-         *Begin = *End;
-         *End   = tmp;
-
-         Begin++;
-         End--;
-    }
-}
-
-char* ReverseSentence(char* Data)
-{
-    if(nullptr == Data)
-        return nullptr;
-
-    char* Begin = Data;
-    char* End = Data;
-    while(*End != '\0')
-        End++;
-    End--;
-
-    //反转整个句子
-    Reverse(Begin,End);
-
-    //反转句子中的每个单词
-    Begin = End = Data;
-    while(*Begin != '\0'){
-        if(*Begin == ' '){
-            Begin++;
-            End++;
-        }else if(*End == ' ' || *End == '\0'){
-            Reverse(Begin,End);
-            Begin = ++End;
-        }else
-            End++;
-    }
-
-    return Data;
-}
-
-
-char* LeftRotateString(char* Str,int n)
-{
-    if(Str != nullptr){
-        int length = static_cast<int>(strlen(Str));
-        if(length > 0 && n > 0 && n < length){
-            char* FirstStart = Str;
-            char* FirstEnd = Str+n-1;
-            char* SecondStart = Str+n;
-            char* SecondEnd = Str+length-1;
-
-            Reverse(FirstStart,FirstEnd);
-
-            Reverse(SecondStart,SecondEnd);
-
-            Reverse(FirstStart,SecondEnd);
-        }
-    }
-
-    return Str;
-}
+// void Reverse(char* Begin,char* End)
+// {
+//     if(nullptr == Begin || nullptr == End)
+//         return;
+// 
+//     while(Begin < End){
+//          char tmp = *Begin;
+//          *Begin = *End;
+//          *End   = tmp;
+// 
+//          Begin++;
+//          End--;
+//     }
+// }
+// 
+// char* ReverseSentence(char* Data)
+// {
+//     if(nullptr == Data)
+//         return nullptr;
+// 
+//     char* Begin = Data;
+//     char* End = Data;
+//     while(*End != '\0')
+//         End++;
+//     End--;
+// 
+//     //反转整个句子
+//     Reverse(Begin,End);
+// 
+//     //反转句子中的每个单词
+//     Begin = End = Data;
+//     while(*Begin != '\0'){
+//         if(*Begin == ' '){
+//             Begin++;
+//             End++;
+//         }else if(*End == ' ' || *End == '\0'){
+//             Reverse(Begin,End);
+//             Begin = ++End;
+//         }else
+//             End++;
+//     }
+// 
+//     return Data;
+// }
+// 
+// 
+// char* LeftRotateString(char* Str,int n)
+// {
+//     if(Str != nullptr){
+//         int length = static_cast<int>(strlen(Str));
+//         if(length > 0 && n > 0 && n < length){
+//             char* FirstStart = Str;
+//             char* FirstEnd = Str+n-1;
+//             char* SecondStart = Str+n;
+//             char* SecondEnd = Str+length-1;
+// 
+//             Reverse(FirstStart,FirstEnd);
+// 
+//             Reverse(SecondStart,SecondEnd);
+// 
+//             Reverse(FirstStart,SecondEnd);
+//         }
+//     }
+// 
+//     return Str;
+// }
 
 class Solution{
 public:
@@ -113,3 +112,10 @@ private:
         }
     }
 };
+
+int main(){
+    Solution s;
+    string str = "abcdef";
+    string res = s.LeftRotateString(str,2);
+    cout<<res<<endl;
+}
