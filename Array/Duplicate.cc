1a193116049d146300d09ceaa96b0ad2d000acbc
treasureb <92856540@qq.com>
2018-06-05 22:38:22 +0800

    数组中重复的数字，采用哈希思想，每个数对应到自己的下标处

--- Array/Duplicate.cc
+++ Array/Duplicate.cc
@@ -7,8 +7,12 @@ class Solution{
 
         for(int i = 0;i < length;++i){
             int index = numbers[i];
+
+            //超过length,说明是该下标的数已经出现一次
             if(index >= length)
                 index -= length;
+
+            //说明值为index的数已经出现过一次了
             if(numbers[index] >= length){
                 duplicate[0] = index;
                 return true;
