8d5eab25c400634f1cad559048382da4a7d5e482
treasureb <92856540@qq.com>
2018-06-11 09:58:57 +0800

    左旋链表,注意旋转的K值取模

--- List/rotate_right.cc
+++ List/rotate_right.cc
@@ -13,6 +13,7 @@ public:
             cur = cur->_next;
         }
 
+        //k的值可能超过链表的长度，所以要取余
         k %= len;
         Node* fast = head;
         Node* slow = head;
@@ -21,16 +22,20 @@ public:
                fast = fast->_next;
         }
 
+        //如果快指针等于空，说明链表长度刚好为K
         if(fast == nullptr)
             return head;
+        //利用快慢指针找到需要旋转的节点
         while(fast->_next){
             fast = fast->_next;
             slow = slow->_next;
         }
+        //将链表尾部链接到头部
         fast->_next = head;
         
         //相当于新链表的头节点
         fast = slow->_next;
+        //将原链表处断开
         slow->_next = nullptr;
 
         return fast;
