25eabbdd4e58fa449d648af4f8a58200f6ab1a78
treasureb <929856540@qq.com>
2018-06-19 23:33:32 +0800

    用递归函数和栈操作,逆置一个栈

---
+++ StackAndQueue/reverseStackR.cc
@@ -0,0 +1,30 @@
+#include<vector>
+using namespace std;
+
+class Solution{
+public:
+    vector<int> reverse_stack_R(vector<int> stack,int top){
+        if(top == 1)
+            return stack;
+
+        int tmp = stack.back();              //最上面的数出栈
+        stack.pop_back();
+        stack = reverse_stack_R(stack,top-1);//递归调用
+        stack = insertToBottom(stack,tmp);   //用一个函数实现将一个数插入到最底层
+        return stack;
+    }
+private:
+    vector<int> insertToBottom(vector<int> stack,int num){
+        if(stack.size() == 0){               //当栈为空的时候,将该数放入栈底
+            stack.push_back(num);
+            return stack;
+        }
+
+        int top = stack.back();             
+        stack.pop_back();                   //将最上面的数出栈保存 
+        stack = insertToBottom(stack,num);  //把数插入到最底层
+        stack.push_back(top);               //再把原先的最上面的数入栈
+
+        return stack;
+    }
+};
