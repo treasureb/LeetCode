183e5d4ab16ce8768cf3ffbc23b09063bce1920a
treasureb <929856540@qq.com>
2018-06-24 23:14:05 +0800

    90°逆置二维矩阵

--- Array/RotateImage.cc
+++ Array/RotateImage.cc
@@ -7,11 +7,11 @@ public:
         if(matrix.empty())
             return;
 
-        size_t s = matrix.size();
+        size_t s = matrix[0].size();
         vector<vector<int>> tmp(s,vector<int>(s,0));
         for(size_t i = 0;i < s;++i){
             for(size_t j = 0;j < s;++j)
-                tmp[j][s-1-j] = matrix[i][j];
+                tmp[j][s-1-i] = matrix[i][j];
         }
         matrix = tmp;
     }
