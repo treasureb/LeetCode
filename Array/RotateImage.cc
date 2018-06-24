77e45601205cbf2f03f091079b9960b990a40d40
treasureb <929856540@qq.com>
2018-06-24 22:44:09 +0800

    90°逆置一个二维矩阵

---
+++ Array/RotateImage.cc
@@ -0,0 +1,18 @@
+#include<vector>
+using namespace std;
+
+class Solution{
+public:
+    void rotate(vector<vector<int>>& matrix){
+        if(matrix.empty())
+            return;
+
+        size_t s = matrix.size();
+        vector<vector<int>> tmp(s,vector<int>(s,0));
+        for(size_t i = 0;i < s;++i){
+            for(size_t j = 0;j < s;++j)
+                tmp[j][s-1-j] = matrix[i][j];
+        }
+        matrix = tmp;
+    }
+};
