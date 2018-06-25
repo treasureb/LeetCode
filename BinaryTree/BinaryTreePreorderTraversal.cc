1759a008f0f035f85123aae633665ee400018f62
treasureb <929856540@qq.com>
2018-06-25 15:07:52 +0800

    三种方法实现求前序遍历

--- BinaryTree/BinaryTreePreorderTraversal.cc
+++ BinaryTree/BinaryTreePreorderTraversal.cc
@@ -9,7 +9,7 @@ public:
      * 1.采用栈来保存沿途的节点
      * 2.先判断右节点是否存在，这样左节点就可以先出栈
      */
-    vector<int> preorderTraversal(TreeNode* root){
+    vector<int> preorderTraversal1(TreeNode* root){
         vector<int> res;
         if(!root)
             return res;
@@ -27,6 +27,31 @@ public:
         return res;
     } 
 
+
+    vector<int> preorderTraversal2(TreeNode* root){
+        vector<int> res;
+        if(root == nullptr)
+            return res;
+
+        stack<TreeNode*> s;
+        while(root || !s.empty()){
+            //沿途保存左子树
+            while(root){
+                res.push_back(root->value);
+                s.push(root);
+                root = root->left;
+            }
+            //走到最左节点，看是否存在右子树
+            TreeNode* top = s.top();
+            s.pop();
+            root = top->right;
+        }
+        return res;
+    }
+
+    /* 递归求二叉树的前序遍历
+     *
+     */
     vector<int> preorderTraversal_R(TreeNode* root){
         vector<int> res;
         preorder_R(root,res);
