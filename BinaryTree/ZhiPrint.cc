04ebfd94cc5dac48eede9df9fcbe915e68b8911f
treasureb <929856540@qq.com>
2018-06-26 16:15:58 +0800

    层序遍历和之字形打印二叉树

--- BinaryTree/ZhiPrint.cc
+++ BinaryTree/ZhiPrint.cc
@@ -6,34 +6,34 @@
 
 #include"Tree.h"
 
-void ZhiPrint(Node* Head)
+void ZhiPrint(TreeNode* Head)
 {
     if(nullptr == Head)
         return;
 
-    stack<Node*> levels[2];
+    stack<TreeNode*> levels[2];
     //标志奇数行和偶数行
     int current = 0;
     int next = 1;
     
     levels[current].push(Head);
     while(!levels[current].empty() && !levels[next].empty()){
-        Node* top = levels[current].top();
+        TreeNode* top = levels[current].top();
         levels[current].pop();
 
-        cout<<top->_value<<" ";
+        cout<<top->value<<" ";
         
         //偶数行，先入左子树
         if(current == 0){
-            if(top->_left)
-                levels[next].push(top->_left);
-            if(top->_right)
-                levels[next].push(top->_right);
+            if(top->left)
+                levels[next].push(top->left);
+            if(top->right)
+                levels[next].push(top->right);
         }else{//奇数行，先入右子树
-            if(top->_right)
-                levels[next].push(top->_right);
-            if(top->_left)
-                levels[next].push(top->_left);
+            if(top->right)
+                levels[next].push(top->right);
+            if(top->left)
+                levels[next].push(top->left);
         }
 
         //当前行打印完，要更新奇数行和偶数行
@@ -47,12 +47,12 @@ void ZhiPrint(Node* Head)
 
 class Solution{
 public:
-    vector<vector<int>> Print(Node* pRoot){
+    vector<vector<int>> Print(TreeNode* pRoot){
         vector<vector<int>> res;
         if(nullptr == pRoot)
             return res;
 
-        queue<Node*> q;
+        queue<TreeNode*> q;
         q.push(pRoot);
         bool even = false;
         while(!q.empty()){
@@ -60,13 +60,13 @@ public:
             int level_number = q.size();
             int i = 0;
             while(i < level_number){
-                Node* f = q.front();
+                TreeNode* f = q.front();
                 q.pop();
-                v.push_back(f->_value);
-                if(f->_left)
-                    q.push(f->_left);
-                if(f->_right)
-                    q.push(f->_right);
+                v.push_back(f->value);
+                if(f->left)
+                    q.push(f->left);
+                if(f->right)
+                    q.push(f->right);
 
                 ++i;
             }
