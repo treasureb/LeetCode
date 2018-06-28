e7079c2f32b3e457b4cafaa298dfa33c06ee4506
treasureb <929856540@qq.com>
2018-06-28 22:16:46 +0800

    两个相等的二叉树判断

--- BinaryTree/SameTree.cc
+++ BinaryTree/SameTree.cc
@@ -1,5 +1,11 @@
 #include"Tree.h"
 
+/*
+ * 判断两个二叉树是否相等
+ * 1.如果都不为空，则判断当前节点的数值和其左右子树
+ * 2.如果一个为空，一个不为空直接返回flase
+ * 3.否则返回true
+ */
 class Solution{
 public:
     bool isSameTree(TreeNode* p,TreeNode* q){
