c94851a20b245bd2fd794aea4f93261bd2a50d24
treasureb <929856540@qq.com>
2018-08-31 14:51:52 +0800

    连续的最长子序列问题

--- DP/LongestConsecutiveSequence/LCS.cc
+++ DP/LongestConsecutiveSequence/LCS.cc
@@ -5,6 +5,13 @@ using namespace std;
 
 class Solution{
 public:
+    
+    /* 解题思路: */
+    /*         1.使用set保存原有数组的所有节点 */
+    /*         2.依次便利原有数组,然后找其左边和右边的数字 */
+    /*         3.在set中查找与其相邻的数字然后删除,当前最长序列+1 */
+    /*         4.最后得出整个序列中最长的子序列 */
+
     int longestConsecutive(vector<int>& nums){
         int max_res = 0;
         unordered_set<int> us(nums.begin(),nums.end());
