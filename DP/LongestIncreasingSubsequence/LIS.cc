61432c8320c33ad0ada21667524e9ba7477ff2c3
treasureb <929856540@qq.com>
2018-09-03 18:22:21 +0800

    最大上升子序列问题

--- DP/LongestIncreasingSubsequence/LIS.cc
+++ DP/LongestIncreasingSubsequence/LIS.cc
@@ -6,6 +6,8 @@ using namespace std;
 
 class Solution{
 public:
+    
+    /* 贪心+二分,时间复杂度为O(nlogn) */
     int lengthOfLIS(vector<int>& nums){
         if(nums.empty())
             return 0;
@@ -22,5 +24,18 @@ public:
             }
         }
         return pos+1;
+    }
+
+    int lengthOfLIS2(vector<int>& nums){
+        vector<int> dp(nums.size(),1);
+        int max_res = 1;
+        for(int i = 0;i < (int)nums.size();++i){
+            for(int j = 0;j < i;++j){   //扫描0-i中符合的边界
+                dp[i] = max(dp[i],dp[j]+1);
+            }
+            max_res = max(dp[i],max_res);
+        }
+
+        return max_res;
     }
 };
