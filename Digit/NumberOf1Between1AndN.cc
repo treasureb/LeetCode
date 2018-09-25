6b62abb214d0b319cd725e94b39dfb41f7583306
treasureb <929856540@qq.com>
2018-09-25 09:49:54 +0800

    求1-N之间1的个数，采用计算每一位上的个数

--- Digit/NumberOf1Between1AndN.cc
+++ Digit/NumberOf1Between1AndN.cc
@@ -22,6 +22,27 @@ public:
         }
         return count;
     }
+
+
+    /*
+     * 2. 采用计算每一位上1的个数
+     *
+     */
+    int NumberOf1Between1AndN2(int n){
+        int exp = 1;
+        int ans = 0;
+        while(n / exp){
+            ans += n / (exp * 10) * exp;
+            if(n % (exp * 10) / exp > 1)
+                ans += exp;
+            else if(n % (exp * 10) / exp == 1)
+                ans += (n % exp + 1);
+
+            exp *= 10;
+        }
+
+        return ans;
+    }
 };
 
 int main()
