c3175a027c1af6fc41cbcc6d3d7711ffdb6f7b5b
treasureb <929856540@qq.com>
2019-02-20 18:53:43 +0800

    连续子数组最大和

--- Array/FindGreatSumOfSubArray.cc
+++ Array/FindGreatSumOfSubArray.cc
@@ -3,43 +3,20 @@
 
 using namespace std;
 
-int FindGreatSumOfSubArray(int* Data,int length)
-{
-    if(nullptr == Data || length <= 0)
-        return -1;
-
-    int CurSum = 0;
-    int GreatestSum = 0x80000000;
-    for(int i = 0;i < length;i++){
-        //如果当前和小于0，就丢弃前面的值
-        if(CurSum < 0)
-            CurSum = Data[i];
-        else
-            //前面的和大于0，则加上当前的值
-            CurSum += Data[i];
-
-        //更新最大的子数组和
-        if(CurSum > GreatestSum)
-            GreatestSum = CurSum;
-    }
-
-    return GreatestSum;
-}
-
 class Solution{
     int FindGreatSumOfSubArray(vector<int> array){
         if(array.empty())
             return 0;
 
-        int sum = array[0];
-        int tmpsum = array[0];
+        int max_sum = array[0];
+        int cur_sum = array[0];
         for(int i = 1;i < (int)array.size();++i){
             //如果当前最大和为负数，则将当前最大值更新为当前值
-            tmpsum = (tmpsum < 0) ? array[i] : tmpsum + array[i];
+            cur_sum = (array[i] < 0) ? array[i] : cur_sum + array[i];
             //如果当前最大值大于最大值，则更新最大值
-            sum = (tmpsum > sum)? tmpsum : sum;
+            max_sum = (cur_sum > max_sum)? cur_sum : max_sum;
         }
 
-        return sum;
+        return max_sum;
     }    
 };
