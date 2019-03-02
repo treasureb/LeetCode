c8ec02ba1472d3847302761705668fe1483bc337
treasureb <929856540@qq.com>
2019-03-02 14:01:00 +0800

    反转字符串

---
+++ String/Reverse.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+//将一个字符串进行逆置
+void Reverse(char* Begin,char* End)
+ {
+     if(NULL == Begin || NULL == End)
+         return;
+ 
+     while(Begin < End){
+          char tmp = *Begin;
+          *Begin = *End;
+          *End   = tmp;
+ 
+          Begin++;
+          End--;
+     }
+ }
+ 
+ char* ReverseSentence(char* Data)
+ {
+     if(NULL == Data)
+         return NULL;
+ 
+     char* Begin = Data;
+     char* End = Data;
+     while(*End != '\0')
+         End++;
+     End--;
+ 
+     //反转整个句子
+     Reverse(Begin,End);
+ 
+     //反转句子中的每个单词
+     Begin = End = Data;
+     while(*Begin != '\0'){
+         if(*Begin == ' '){
+             Begin++;
+             End++;
+         }else if(*End == ' ' || *End == '\0'){
+             Reverse(Begin,End-1);
+             Begin = ++End;
+         }else
+             End++;
+     }
+ 
+     return Data;
+ }
+
+int main(){
+    char str[20] = "You And I";
+    ReverseSentence(str);
+    printf("%s\n",str);
+    return 0;
+}
