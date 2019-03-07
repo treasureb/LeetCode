ac83815af0a9c10f93c3bea8fbb5cc7197c0766f
treasureb <929856540@qq.com>
2019-03-07 18:30:20 +0800

    最小窗口子串

--- String/MinimumWindowSubstr.cc
+++ String/MinimumWindowSubstr.cc
@@ -4,6 +4,14 @@
 
 using  namespace std;
 
+
+/* 解题思路: */
+/*     1. 先将子串全部放入map中,以备后续判断其是否出现 */
+/*     2. 当子串中的字母在待查找串中出现则计数器++ */
+/*     3. 当计数器的值等于子串长度说明子串中的字母已经全部出现 */
+/*     4. 判断是否需要更新窗口的大小 */
+/*     5. 窗口滑动是判断是否失去一个子串中出现的字符 */
+
 class Solution{
     string wondow_substr(string s,string t){
         string res;
