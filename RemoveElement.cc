ee33a19afc3fbdccded07470eb62a400f5297fd5
treasureb <929856540@qq.com>
2019-03-31 12:13:50 +0800

    删除数组中值为val的值，返回删除的个数

--- RemoveElement.cc
+++ RemoveElement.cc
@@ -1,6 +1,11 @@
 #include<iostream>
 #include<vector>
 
+
+/* 解题思路: */
+/*     1. 采用s标记value出现的位置 */
+/*     2. 然后继续往后走,将正确的值与该位置的数字交换 */
+    
 class Solution
 {
     int removeElement(std::vector<int>& v,int value)
