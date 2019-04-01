bf8ce4fcb44068d0461547610e890e8403aaad52
treasureb <929856540@qq.com>
2019-04-01 14:16:39 +0800

    利用哈希求解数列中和为target的两个数

--- Hash/2sum.cc
+++ Hash/2sum.cc
@@ -17,28 +17,15 @@ vector<int> TwoSum(vector<int>& nums,int target)
 
     vector<int> ans(2);
 
-    for(size_t i  = 0;i < nums.size();++i)
+    for(int i  = 0;i < (int)nums.size();++i)
     {
         if(Hash.find(target-nums[i]) != Hash.end() && Hash[target-nums[i]] != i)
         {
-            ans[0] = i+1;
-            ans[1] = Hash[target-nums[i]] +1;
+            ans[0] = i;
+            ans[1] = Hash[target-nums[i]];
             break;
         }
     }
     return ans;
 }
 
-int main()
-{
-    vector<int> v = {2,4,6,8,3,14};
-    vector<int> ret = TwoSum(v,17);
-    vector<int>::iterator it = ret.begin();
-    while(it != ret.end())
-    {
-        cout<<*it<<" ";
-        ++it;
-    }
-    cout<<endl;
-    return 0;
-}
