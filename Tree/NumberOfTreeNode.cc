4c1a78fff2eb295d5ec05236f41c8c81ddb99a24
treasureb <929856540@qq.com>
2019-04-04 21:55:59 +0800

    递归求解各种节点个数问题

--- Tree/NumberOfTreeNode.cc
+++ Tree/NumberOfTreeNode.cc
@@ -10,3 +10,29 @@ int NumberOfTreeNode(TreeNode* root){
 
     return left + right + 1;
 }
+
+/* 求解二叉树中叶子节点的个数 */
+int NumberOfTreeLeafNode(TreeNode* root){
+    if(root == nullptr)
+        return 0;
+
+    if(root->left == nullptr && root->right == nullptr)
+        return 1;
+
+    return NumberOfTreeNode(root->left) + NumberOfTreeNode(root->right);
+}
+
+/* 求解二叉树中第K层节点的个数 */
+int NumberOfTreeKLevelNode(TreeNode* root,int k){
+    if(root == nullptr || k < 1)
+        return 0;
+
+    if(k == 1)
+        return 1;
+
+    int left = NumberOfTreeKLevelNode(root->left,k-1);
+    int right = NumberOfTreeKLevelNode(root->right,k-1);
+
+    return left + right;
+}
+
